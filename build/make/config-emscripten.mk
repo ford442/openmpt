b13486f387390becc3a0395a0d7aca381f9b1036
Noah Cohn <noahc42@gmail.com>
2024-05-21 04:58:36 -0700

    Update config-emscripten.mk

--- build/make/config-emscripten.mk
+++ build/make/config-emscripten.mk
@@ -20,7 +20,7 @@ EMSCRIPTEN_PORTS?=0
 ifneq ($(STDCXX),)
 CXXFLAGS_STDCXX = -std=$(STDCXX)
 else ifeq ($(shell printf '\n' > bin/empty.cpp ; if $(CXX) -std=c++20 -c bin/empty.cpp -o bin/empty.out > /dev/null 2>&1 ; then echo 'c++20' ; fi ), c++20)
-CXXFLAGS_STDCXX = -std=c++20
+CXXFLAGS_STDCXX = -std=c++2b
 else
 CXXFLAGS_STDCXX = -std=c++17
 endif
@@ -67,14 +67,14 @@ CFLAGS   += -Oz
 LDFLAGS  += -Oz
 
 # Enable LTO as recommended by Emscripten
-CXXFLAGS += -flto=thin
-CFLAGS   += -flto=thin
-LDFLAGS  += -flto=thin -Wl,--thinlto-jobs=all
+#CXXFLAGS += -flto=thin
+#CFLAGS   += -flto=thin
+#LDFLAGS  += -flto=thin -Wl,--thinlto-jobs=all
 # As per recommendation in <https://github.com/emscripten-core/emscripten/issues/15638#issuecomment-982772770>,
 # thinLTO is not as well tested as full LTO. Stick to full LTO for now.
-#CXXFLAGS += -flto
-#CFLAGS   += -flto
-#LDFLAGS  += -flto
+CXXFLAGS += -flto
+CFLAGS   += -flto
+LDFLAGS  += -flto
 
 ifeq ($(EMSCRIPTEN_TARGET),default)
 # emits whatever is emscripten's default, currently (13.1.51) this is the same as "wasm" below.
