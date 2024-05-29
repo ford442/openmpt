fecf93a967e538aec79e6059dcc36d9d267c55e0
Noah Cohn <noahc42@gmail.com>
2024-05-29 12:16:24 -0700

    Update config-emscripten.mk

--- build/make/config-emscripten.mk
+++ build/make/config-emscripten.mk
@@ -137,11 +137,13 @@ SIMD_FLAGS = -DSIMD=AVX -msimd128 -mavx
 CPPFLAGS += -ffp-contract=off -fno-fast-math
 CXXFLAGS += -ffp-contract=off -fno-fast-math
 CFLAGS   += -ffp-contract=off -fno-fast-math
-LDFLAGS  += $(LINK_SIMD_FLAGS) -DNDEBUG=1 -sTRUSTED_TYPES=1 -pipe -dead-strip -march=wasm32-avx -fno-fast-math \
+LDFLAGS  += $(LINK_SIMD_FLAGS) -DNDEBUG=1 -sTRUSTED_TYPES=1 -pipe -dead-strip -fno-fast-math \
 -mtune=wasm32 -polly -polly-position=before-vectorizer -ffp-contract=off -fexcess-precision=fast -stdlib=libc++ \
 -sALLOW_UNIMPLEMENTED_SYSCALLS=1 -mextended-const -mbulk-memory --typed-function-references --enable-reference-types \
 -matomics -mmutable-globals -msign-ext -fmerge-all-constants -fno-omit-frame-pointer \
--sWASM=0 -sFORCE_FILESYSTEM=1 -sALLOW_MEMORY_GROWTH=0 -sINITIAL_MEMORY=700mb -sALLOW_TABLE_GROWTH
+-sWASM=0 -sFORCE_FILESYSTEM=1 -sALLOW_MEMORY_GROWTH=0 -sINITIAL_MEMORY=700mb -sALLOW_TABLE_GROWTH \
+--closure 0 --closureFriendly -rtlib=compiler-rt \
+--output_eol linux -sAUTO_ARCHIVE_INDEXES=0 -sALLOW_UNIMPLEMENTED_SYSCALLS=0 -sIGNORE_MISSING_MAIN=0 -sASSERTIONS=0
 
 else ifeq ($(EMSCRIPTEN_TARGET),js)
 # emits only plain javascript with plain javascript focused optimizations.
