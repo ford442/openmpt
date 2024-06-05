adb8f7a3adf40fd7b156a58fdc9e696bf34ab0f7
Noah Cohn <noahc42@gmail.com>
2024-06-05 00:55:05 -0700

    Update config-emscripten.mk

--- build/make/config-emscripten.mk
+++ build/make/config-emscripten.mk
@@ -143,7 +143,8 @@ LDFLAGS  += $(LINK_SIMD_FLAGS) -DNDEBUG=1 -sEMULATE_FUNCTION_POINTER_CASTS=1 -sT
 -matomics -mmutable-globals -msign-ext -fmerge-all-constants -fno-omit-frame-pointer \
 -sWASM=0 -sFORCE_FILESYSTEM=1 -sALLOW_MEMORY_GROWTH=0 -sINITIAL_MEMORY=700mb -sALLOW_TABLE_GROWTH \
 -march=haswell -rtlib=compiler-rt -sLEGALIZE_JS_FFI=1 -sENVIRONMENT=web -WASMFS=1 \
---output_eol linux --use-preload-plugins --closure 0 --closureFriendly
+--output_eol linux --use-preload-plugins --closure 0 --closureFriendly \
+-Wl,-O3,-lc++,-lc++abi,-lm,-ldl
 
 else ifeq ($(EMSCRIPTEN_TARGET),js)
 # emits only plain javascript with plain javascript focused optimizations.
