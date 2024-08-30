573bd4dbe8b236aeec20acd323956713141005c9
Noah Cohn <noahc42@gmail.com>
2024-08-29 22:06:50 -0700

    Update config-emscripten.mk

--- build/make/config-emscripten.mk
+++ build/make/config-emscripten.mk
@@ -143,7 +143,7 @@ LDFLAGS  += $(LINK_SIMD_FLAGS) -DNDEBUG=1 -sEMULATE_FUNCTION_POINTER_CASTS=1 \
 -mextended-const -mbulk-memory --typed-function-references --enable-reference-types \
 -matomics -mmutable-globals -msign-ext -fmerge-all-constants -fno-omit-frame-pointer \
 -sWASM=0 -sFORCE_FILESYSTEM=1 -sALLOW_MEMORY_GROWTH=0 -sINITIAL_MEMORY=700mb -sALLOW_TABLE_GROWTH \
--march=haswell -rtlib=compiler-rt -sWASM_BIGINT=1 -sENVIRONMENT=web -sASYNCIFY=0 \
+-march=haswell -rtlib=compiler-rt -sLEGALIZE_JS_FFI=1 -sENVIRONMENT=web -sASYNCIFY=0 \
 --output_eol linux --use-preload-plugins --closure 0 --closureFriendly -sASYNCIFY=0 -sSTRICT_JS=0
 
 
