356543a83362fc99677ba84beb5862fa9bbb5593
Noah Cohn <noahc42@gmail.com>
2024-09-20 23:34:35 -0700

    Update config-emscripten.mk

--- build/make/config-emscripten.mk
+++ build/make/config-emscripten.mk
@@ -128,10 +128,10 @@ LDFLAGS += -s ALLOW_MEMORY_GROWTH=1
 else ifeq ($(EMSCRIPTEN_TARGET),1it1-new)
 LINK_SIMD_FLAGS = -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -mavx -msimd128
 SIMD_FLAGS = -DSIMD=AVX -msimd128 -mavx
-CPPFLAGS += -ffp-contract=off -fno-fast-math $(SIMD_FLAGS) -ffp-contract=off -fexcess-precision=standard 
-CXXFLAGS += -ffp-contract=off -fno-fast-math $(SIMD_FLAGS) -ffp-contract=off -fexcess-precision=standard 
-CFLAGS   += -ffp-contract=off -fno-fast-math $(SIMD_FLAGS) -ffp-contract=off -fexcess-precision=standard 
-LDFLAGS  += $(LINK_SIMD_FLAGS) -DNDEBUG=1 -sEMULATE_FUNCTION_POINTER_CASTS=1 \
+CPPFLAGS += -ffp-contract=off -fno-fast-math  -ffp-contract=off -fexcess-precision=standard 
+CXXFLAGS += -ffp-contract=off -fno-fast-math  -ffp-contract=off -fexcess-precision=standard 
+CFLAGS   += -ffp-contract=off -fno-fast-math  -ffp-contract=off -fexcess-precision=standard 
+LDFLAGS  += -DNDEBUG=1 -sEMULATE_FUNCTION_POINTER_CASTS=1 \
 -sTRUSTED_TYPES=1 -pipe -dead-strip -fno-fast-math -mtune=wasm32 -polly -polly-position=before-vectorizer \
 -ffp-contract=off -fexcess-precision=standard -stdlib=libc++ -sALLOW_UNIMPLEMENTED_SYSCALLS=1 \
 -mextended-const -mbulk-memory --typed-function-references --enable-reference-types \
