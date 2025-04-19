bbaf4a8ccd5b7df4167e2ca0b8a2714c0555f6ad
Noah Cohn <noahc42@gmail.com>
2025-04-19 03:38:13 -0700

    Update config-emscripten.mk

--- build/make/config-emscripten.mk
+++ build/make/config-emscripten.mk
@@ -136,7 +136,7 @@ LDFLAGS  += -DNDEBUG=1 \
 -ffp-contract=off -fexcess-precision=standard -stdlib=libc++ -sALLOW_UNIMPLEMENTED_SYSCALLS=1 \
 -mextended-const -mbulk-memory --typed-function-references --enable-reference-types \
 -matomics -mmutable-globals -msign-ext -fmerge-all-constants -fno-omit-frame-pointer \
--sWASM=0 -sFORCE_FILESYSTEM=1 -sALLOW_MEMORY_GROWTH=0 -sINITIAL_MEMORY=700mb -sALLOW_TABLE_GROWTH \
+-sWASM=0 -sFORCE_FILESYSTEM=1 -sALLOW_MEMORY_GROWTH=0 -sINITIAL_MEMORY=700mb -sMALLOC='mimalloc' \
 -march=haswell -rtlib=compiler-rt -sENVIRONMENT=web -sASYNCIFY=0 -sEXIT_RUNTIME=0 \
 --output_eol linux --use-preload-plugins --closure 0 --closureFriendly -sSTRICT_JS=0
 
