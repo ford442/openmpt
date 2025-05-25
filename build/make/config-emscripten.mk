6e0abeed38c1f293ca00e71032f319e805f1e6fc
Noah Cohn <noahc42@gmail.com>
2025-05-24 23:01:00 -0700

    Update config-emscripten.mk

--- build/make/config-emscripten.mk
+++ build/make/config-emscripten.mk
@@ -128,18 +128,18 @@ LDFLAGS += -s ALLOW_MEMORY_GROWTH=1
 
 
 else ifeq ($(EMSCRIPTEN_TARGET),1it1-new2)
-LINK_SIMD_FLAGS = -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -mavx -msimd128 -mavx2 -mrelaxed-simd
+LINK_SIMD_FLAGS = --enable-simd -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -mavx -msimd128 -mavx2 -mrelaxed-simd
 SIMD_FLAGS = -DSIMD=AVX -msse4.2 -msimd128 -mavx2 -mrelaxed-simd
-CPPFLAGS += -fno-fast-math -ffp-contract=off -fexcess-precision=standard -fno-math-errno -mextended-const -mbulk-memory -matomics -mmutable-globals -msign-ext -fmerge-all-constants
-CXXFLAGS += -fno-fast-math -ffp-contract=off -fexcess-precision=standard -fno-math-errno -mextended-const -mbulk-memory -matomics -mmutable-globals -msign-ext -fmerge-all-constants
-CFLAGS   += -fno-fast-math -ffp-contract=off -fexcess-precision=standard -fno-math-errno -mextended-const -mbulk-memory -matomics -mmutable-globals -msign-ext -fmerge-all-constants
+CPPFLAGS += -fno-fast-math -ffp-contract=off -fexcess-precision=precise -fno-math-errno -mextended-const -mbulk-memory -matomics -mmutable-globals -msign-ext -fmerge-all-constants
+CXXFLAGS += -fno-fast-math -ffp-contract=off -fexcess-precision=precise -fno-math-errno -mextended-const -mbulk-memory -matomics -mmutable-globals -msign-ext -fmerge-all-constants
+CFLAGS   += -fno-fast-math -ffp-contract=off -fexcess-precision=precise -fno-math-errno -mextended-const -mbulk-memory -matomics -mmutable-globals -msign-ext -fmerge-all-constants
 LDFLAGS  += -DNDEBUG=1 \
--sTRUSTED_TYPES=1 -pipe -dead-strip -fno-fast-math -mtune=wasm32 -polly -polly-position=before-vectorizer \
--ffp-contract=off -fexcess-precision=standard -stdlib=libc++ -sALLOW_UNIMPLEMENTED_SYSCALLS=1 \
--mextended-const -mbulk-memory --typed-function-references --enable-reference-types -fno-math-errno \
--matomics -mmutable-globals -msign-ext -fmerge-all-constants \
+-sTRUSTED_TYPES=1 -pipe -dead-strip -mtune=wasm32 -polly -polly-position=before-vectorizer \
+-ffp-contract=off -fexcess-precision=precise -stdlib=libc++ -sALLOW_UNIMPLEMENTED_SYSCALLS=1 \
+-fno-fast-math -mextended-const -mbulk-memory --typed-function-references --enable-reference-types \
+-matomics -mmutable-globals -msign-ext -fmerge-all-constants -fno-math-errno \
 -sWASM=0 -sFORCE_FILESYSTEM=1 -sALLOW_MEMORY_GROWTH=0 -sINITIAL_MEMORY=384mb \
--rtlib=compiler-rt -sENVIRONMENT=web -sASYNCIFY=0 -sMALLOC='emmalloc' -sAUDIO_WORKLET=1 \
+-rtlib=compiler-rt -sENVIRONMENT=web -sASYNCIFY=0 -sMALLOC='emmalloc' \
 --output_eol linux --use-preload-plugins --closure 0 --closureFriendly -sSTRICT_JS=0
 
 
