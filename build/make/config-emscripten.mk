cdab1649ec65abf0861e4c42e221a870fd310669
Noah Cohn <noahc42@gmail.com>
2025-06-12 02:59:29 -0700

    Update config-emscripten.mk

--- build/make/config-emscripten.mk
+++ build/make/config-emscripten.mk
@@ -138,14 +138,14 @@ LDFLAGS  += -DNDEBUG=1 \
 -ffp-contract=off -ffp-model=strict -stdlib=libc++ -sALLOW_UNIMPLEMENTED_SYSCALLS=1 \
 -fno-fast-math -mextended-const -mbulk-memory --typed-function-references --enable-reference-types \
 -matomics -mmutable-globals -msign-ext -fmerge-all-constants -fno-math-errno \
--sWASM=0 -sFORCE_FILESYSTEM=1 -sALLOW_MEMORY_GROWTH=0 -sINITIAL_MEMORY=384mb \
+-sWASM=0 -sFORCE_FILESYSTEM=1 -sALLOW_MEMORY_GROWTH=0 -sMAXIMUM_MEMORY=4gb -sINITIAL_HEAP=512mb \
 -rtlib=compiler-rt -sENVIRONMENT=web -sASYNCIFY=0 -sMALLOC='emmalloc' \
 --output_eol linux --use-preload-plugins --closure 0 --closureFriendly -sSTRICT_JS=0
 
 
 else ifeq ($(EMSCRIPTEN_TARGET),1it1-new)
-LINK_SIMD_FLAGS = -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -mavx -msimd128
-SIMD_FLAGS = -DSIMD=AVX -msimd128 -mavx
+LINK_SIMD_FLAGS = -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -mavx -msimd128 -openmp-simd
+SIMD_FLAGS = -DSIMD=AVX -msimd128 -mavx -openmp-simd
 CPPFLAGS += -fno-fast-math -ffp-contract=off -fexcess-precision=standard 
 CXXFLAGS += -fno-fast-math -ffp-contract=off -fexcess-precision=standard 
 CFLAGS   += -fno-fast-math -ffp-contract=off -fexcess-precision=standard 
