4e5354e6ae4646802a10e63a7398114323125ab8
Noah Cohn <noahc42@gmail.com>
2025-07-20 16:45:35 -0700

    Update config-emscripten.mk

--- build/make/config-emscripten.mk
+++ build/make/config-emscripten.mk
@@ -128,8 +128,8 @@ LDFLAGS += -s ALLOW_MEMORY_GROWTH=1
 
 
 else ifeq ($(EMSCRIPTEN_TARGET),1it1-new2)
-LINK_SIMD_FLAGS = --enable-simd -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -mavx -msimd128 -mavx2 -mrelaxed-simd
-SIMD_FLAGS = -DSIMD=AVX -msse4.2 -msimd128 -mavx2 -mrelaxed-simd
+LINK_SIMD_FLAGS = --enable-simd -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -mavx -msimd128 -mavx2 -mrelaxed-simd -openmp-simd
+SIMD_FLAGS = -DSIMD=AVX -msse4.2 -msimd128 -mavx2 -mrelaxed-simd -openmp-simd
 CPPFLAGS += -fno-fast-math -ffp-contract=off -ffp-model=strict -fno-math-errno -mextended-const -mbulk-memory -matomics -mmutable-globals -msign-ext -fmerge-all-constants
 CXXFLAGS += -fno-fast-math -ffp-contract=off -ffp-model=strict -fno-math-errno -mextended-const -mbulk-memory -matomics -mmutable-globals -msign-ext -fmerge-all-constants
 CFLAGS   += -fno-fast-math -ffp-contract=off -ffp-model=strict -fno-math-errno -mextended-const -mbulk-memory -matomics -mmutable-globals -msign-ext -fmerge-all-constants
